f40447e730e39037779146b1dc22e9a305f8c34e
Noisemaker111 <jk10192000@gmail.com>
2025-12-20 16:59:19 -0500

    initial commit

---
+++ packages/backend/convex/auth.ts
@@ -0,0 +1,41 @@
+import { createClient, type GenericCtx } from "@convex-dev/better-auth";
+import { convex, crossDomain } from "@convex-dev/better-auth/plugins";
+import { betterAuth } from "better-auth";
+import { v } from "convex/values";
+
+import { components } from "./_generated/api";
+import { DataModel } from "./_generated/dataModel";
+import { query } from "./_generated/server";
+import authConfig from "./auth.config";
+
+const siteUrl = process.env.SITE_URL!;
+
+export const authComponent = createClient<DataModel>(components.betterAuth);
+
+function createAuth(ctx: GenericCtx<DataModel>) {
+  return betterAuth({
+    trustedOrigins: [siteUrl],
+    database: authComponent.adapter(ctx),
+    emailAndPassword: {
+      enabled: true,
+      requireEmailVerification: false,
+    },
+    plugins: [
+      crossDomain({ siteUrl }),
+      convex({
+        authConfig,
+        jwksRotateOnTokenGenerationError: true,
+      }),
+    ],
+  });
+}
+
+export { createAuth };
+
+export const getCurrentUser = query({
+  args: {},
+  returns: v.any(),
+  handler: async function (ctx, args) {
+    return authComponent.getAuthUser(ctx);
+  },
+});
